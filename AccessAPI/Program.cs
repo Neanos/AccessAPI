16a7158cd41a2746d49e2ee607c228641fec9a50
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2023-02-15 13:29:07 +0100

    fvdfa

--- AccessAPI/Program.cs
+++ AccessAPI/Program.cs
@@ -2,16 +2,32 @@ WebApplication app = WebApplication.Create();
 app.Urls.Add("http://localhost:3000");
 app.Urls.Add("http://*:3000");
 
-superhero hero = new superhero();
-hero.Name = "Superman";
-hero.Power = 9000;
-hero.UnderwearOutside = true;
+List<superhero> heroes = new();
+heroes.Add(new() {Name="Superman", Power=9001});
+heroes.Add(new() {Name="Batman", Power=9});
+heroes.Add(new() {Name="Wallman", Power=-5});
 
 app.MapGet("/", Answer);
 
+
 app.MapGet("/superhero/", () =>
 {
-    return hero;
+    return heroes;
+});
+
+app.MapGet("/superhero/{num}", (int num) =>
+{
+    if( num >= 0 && num < heroes.Count)
+    {
+        return Results.Ok(heroes[num]);
+
+    }
+    return Results.NotFound();
+});
+
+app.MapPost("/superhero", (superhero h) =>
+{
+    Console.WriteLine(h.Name);
 });
 
 app.Run();
